f72618c56f73e0882cc1a306aef8c72eac87cd44
tangming579 <infly123@sina.com>
2014-07-25 18:01:44 +0800

    add html

---
+++ chapter12/scripts/global.js
@@ -0,0 +1,82 @@
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+
+function insertAfter(newElement,targetElement) {
+  var parent = targetElement.parentNode;
+  if (parent.lastChild == targetElement) {
+    parent.appendChild(newElement);
+  } else {
+    parent.insertBefore(newElement,targetElement.nextSibling);
+  }
+}
+
+function addClass(element,value) {
+  if (!element.className) {
+    element.className = value;
+  } else {
+    newClassName = element.className;
+    newClassName+= " ";
+    newClassName+= value;
+    element.className = newClassName;
+  }
+}
+
+function highlightPage() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;  
+  var headers = document.getElementsByTagName('header');
+  if (headers.length == 0) return false;
+  var navs = headers[0].getElementsByTagName('nav');
+  if (navs.length == 0) return false;
+  var links = navs[0].getElementsByTagName("a");
+  for (var i=0; i<links.length; i++) {
+	  var linkurl;
+	  for (var i=0; i<links.length; i++) {
+	    linkurl = links[i].getAttribute("href");
+	    if (window.location.href.indexOf(linkurl) != -1) {
+	      links[i].className = "here";
+	      var linktext = links[i].lastChild.nodeValue.toLowerCase();
+	      document.body.setAttribute("id",linktext);
+	    }
+	  }
+  }
+}
+function showSection(id){
+	var sections=document.getElementsByTagName("section");
+	for (var i = 0; i < sections.length; i++) {
+		if(sections[i].getAttribute("id")!=id){
+			sections[i].style.display="none";
+		}else{
+			sections[i].style.display="block"
+		}
+	};
+}
+function loadEvents() {
+  // home
+  prepareSlideshow();
+  // about
+  prepareInternalnav();
+  // photos
+  preparePlaceholder();
+  prepareGallery();
+  // live
+  stripeTables();
+  highlightRows();
+  displayAbbreviations();
+  // contact
+  focusLabels();
+  prepareForms();
+}
+
+// Load events
+addLoadEvent(highlightPage);
+addLoadEvent(loadEvents);
